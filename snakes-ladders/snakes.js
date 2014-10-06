ef53d0f0a7b130a3a3c7eb13c4921479ea2856c3
joshikumarmanne <joshikumarece@outlook.com>
2014-10-06 11:22:09 +0530

    snakes perfect
    using only function

--- snakes-ladders/snakes.js
+++ snakes-ladders/snakes.js
@@ -19,88 +19,47 @@ $(document).ready(function(){
 	}
 
 	$('.ball').click(function(){
-		//$('.ball').effect( "bounce", {times:3}, 900);	
-		var k = 2;// Math.floor(Math.random()*6 + 1);
+		$('.ball').effect( "bounce", {times:3}, 900);	
+		var k =  Math.floor(Math.random()*6 + 1);
 		$('.ball').text(k);
 
 		if(clicks==1){
-			a=a+k;
-			a=process(a,clicks);
+			a=process(a,k,clicks);
 		}
 		else if(clicks==2)
 		{
-			b=b+k;
-			b=process(b,clicks);}
-			//a=process(a);
-			/*if(ladders[a]) {
-			 a = ladders[a];
-			}
 			
-			if(a==100){
-				$('#' + a).append($('.image1'));
-				alert("Player1 wins the game");
-			}
-			else if(a>100){
-				a=a-k;
-				alert("unable to move");
-			}
-			else{	
-
-				$('#' + a).append($('.image1'));	
-			}
-
-
-		}*/
-
-		
-			//b=process(b);
-			/*if(ladders[b]) {
-			 b = ladders[b];
-			}
-			/*for(var key  in ladders) {
-				if(key==b){
-					b=ladders[key]
-				}
-			
-			if(b==100){
-				$('#' + b).append($('.image2'));
-				alert("Player2 wins the game");
-			}
-			else if(b>100){
-				b=b-k;
-				alert("unable to move");
-			}
-			else{	
-
-				$('#' + b).append($('.image2'));	
-			}} */
-			if([4,6].indexOf(k) < 0){
-				clicks=(clicks < users) ? (clicks+1):1
-			}
-			$('.player').text("Now Player"+clicks);
-			
-
-		});
-});
-var process=function(c,joshi)
-{
-	if(ladders[c]) {
-			 c = ladders[c];
-			}
-			
-			if(c==100){
-				$('#' + c).append($('.image'+joshi));
-				alert("Player1 wins the game");
-			}
-			else if(c>100){
-				c=c-k;
-				alert("unable to move");
-			}
-			else{	
-
-				$('#' + c).append($('.image'+joshi));	
-			}
-return c;
-
+			b=process(b,k,clicks);
 		}
+
+		if([4,6].indexOf(k) < 0){
+			clicks=(clicks < users) ? (clicks+1):1
+		}
+		$('.player').text("Now Player"+clicks);
+
+
+	});
+});
+var process=function(c,h,joshi)
+{
+	c=c+h;
+	if(ladders[c]) {
+		c = ladders[c];
+	}
+
+	if(c==100){
+		$('#' + c).append($('.image'+joshi));
+		alert("Game wins By : player"+joshi );
+	}
+	else if(c>100){
+		c=c-h;
+		alert("unable to move");
+	}
+	else{	
+
+		$('#' + c).append($('.image'+joshi));	
+	}
+	return c;
+
+}
 
