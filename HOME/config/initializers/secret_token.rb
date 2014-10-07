e9a2745797055b70c96efb26b3decf041a21356c
joshikumarmanne <joshikumarece@outlook.com>
2014-10-07 13:12:31 +0530

    snakes on Rails
    Just ntroduction

---
+++ HOME/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snakes::Application.config.secret_key_base = '5feee87993fa0a48b61798562247d77d21a33432233c5f82c7c2e66177b27bc9c639c1cf58c50617bf9c306e59afbc9cc42028558a75847e4ece156998a8032c'
